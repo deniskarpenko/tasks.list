8dd6efa874f321775829a588921d13ef4f5795d5
denis <new.den84@gmail.com>
2020-04-01 15:05:17 +0300

    Add Router

--- app/Router.php
+++ app/Router.php
@@ -9,14 +9,31 @@ class Router
         if ($url === '') {
             
         } else {
-            
+           $controller = $this->_determineController($url); 
+           $method = $controller['method'];
+           $class = new $controller['class'];
+           $class->$method();
         }
-        
-        echo "$url\n\n";
     }
-    
+    /*метод определяющий контроллер и метод по урлу*/
     private function _determineController($url)
     {
-        
+        $controller = explode('/', $url);    
+        if (isset($controller[1])) {
+                $class = '\controller\\'.ucfirst($controller[1]).'Controller';
+                $method = 'actionIndex';     
+                $size = count($controller);
+                if ($size > 3) {
+                    $method = 'action';
+                    for ($i = 2; $i < $size; $i++) {
+                        $method.= ucfirst($controller[$i]);
+                    }
+                }
+                return [
+                    'class' => $class,
+                    'method' => $method,
+                ];
+        }
+        return FALSE;
     }
 }
---
+++ controller/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+namespace controller;
+
+class IndexController
+{
+    public function actionIndex()
+    {
+        echo "\n\n".__METHOD__."\n\n";
+    }
+}
+
